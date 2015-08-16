6a018a0c1185c3f56956bea8b3d12efa9715eb7a
zolij <zolij86@gmail.com>
2015-08-16 23:18:54 +0200

    run apt-get update only if something changed

--- puppet/lamp.pp
+++ puppet/lamp.pp
@@ -1,6 +1,7 @@
-# execute 'apt-get update'
-exec { 'apt-update':                    # exec resource named 'apt-update'
-  command => '/usr/bin/apt-get update'  # command this resource will run
+# Run apt-get update when anything beneath /etc/apt/ changes
+exec { "apt-update":
+  command => "/usr/bin/apt-get update",
+  onlyif => "/bin/sh -c '[ ! -f /var/cache/apt/pkgcache.bin ] || /usr/bin/find /etc/apt/* -cnewer /var/cache/apt/pkgcache.bin | /bin/grep . > /dev/null'",
 }
 
 # install apache2 package
